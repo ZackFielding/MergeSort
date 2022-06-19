52211eac8d4ac87d6335912f470a82052849e194
Zack Fielding <zackfielding@gmail.com>
2022-06-18 20:34:55 -0600

    functional chaining directory generator

--- testOpen.cpp
+++ testOpen.cpp
@@ -6,7 +6,8 @@
 
 // [x] fix multiStrCat
 // windows.h create new dir only works one directory at a time
-// 	[] chain create new dir based on number of folders entered by user
+// [x] chain create new dir based on number of folders entered by user
+// [] test dynamic mem alloc option if user enters very large directory name
 
 void multiStrCat(std::vector<char *> &charVector){ //passbyref vec of char ptrs
 
@@ -22,38 +23,63 @@ void multiStrCat(std::vector<char *> &charVector){ //passbyref vec of char ptrs
 	return;
 };
 
-
-
 void nameDirectory(char *dir_ptr, const int &maxDirSize, bool &dirCheck, LPSECURITY_ATTRIBUTES access,
-		void(*func)(bool &dirCheck, LPSECURITY_ATTRIBUTES access, char *dir_ptr)){
+		void(*func)(bool &, LPSECURITY_ATTRIBUTES, char *, const int&, const int&)){
    		// enter name of new directory folder .. append to dir
 	char folderName [30] {"NNF"}; //no name folder -> default
 	std::cout << "Enter name of new directory folder: " << std::endl;
 	std::cin >> folderName;
 		
-	char backBack[] {"\\"};
+		char backBack[] {"\\"};
 	std::vector <char*> charVector {dir_ptr, backBack, folderName};
+	const int baseDirSize {(int)std::strlen(dir_ptr)};
 
 		// check to see if string sum of both string lengths < dir size
 	if(std::strlen(dir_ptr) + std::strlen(folderName) < maxDirSize){
 		multiStrCat(charVector);
-		func(dirCheck, access, dir_ptr);
+		func(dirCheck, access, dir_ptr, maxDirSize, baseDirSize);
 	}else{
 		char *updatedDir{nullptr};
 		updatedDir = new char [std::strlen(dir_ptr) + std::strlen(folderName) + 5];
-		func(dirCheck, access, updatedDir); //same but passing dynamic alloc dir string
+		//func(dirCheck, access, updatedDir); //UPDATED ONCE NON-DYNAMIC CALL WORKS
 		delete [] updatedDir; // free heap
 	}	
 
 };
 
-void createNewDirectory(bool &dirCheck, LPSECURITY_ATTRIBUTES access, char *dir_ptr){
+void createNewDirectory(bool &dirCheck, LPSECURITY_ATTRIBUTES access, char *dir_ptr,
+	   const int &maxDirSize, const int &baseDirSize){
+
+	char baseDir[maxDirSize] {"nullstr"};
+
+	size_t t {0};
+	for(t ; t <= baseDirSize+1 ; ++t) //include '\'
+		baseDir[t] = dir_ptr[t];
+	
+	const int fullDirLength {(int)std::strlen(dir_ptr)};
 	DWORD threadError {16000}; // local thread error reciever
-	dirCheck = CreateDirectory(dir_ptr, access);	
+	bool firstFolder = true;
+
+	while(t <= fullDirLength){
+		if(!firstFolder){
+			baseDir[t] = '\\'; //replace '\0' with '\'
+			++t;
+		}else
+			firstFolder = false;
+
+		char backBack{'\\'};
+		while(t <= fullDirLength && dir_ptr[t] != backBack){
+			baseDir[t] = dir_ptr[t];	
+			++t;
+		}	
+		baseDir[t] = '\0';
+
+		//push into windows.h create dir func
+		dirCheck = CreateDirectory(baseDir, access);	
 			threadError = GetLastError(); //returns previous threads error code
 			if(threadError != 0){
 				switch (threadError){
-					case 183: std::cerr << "Error: directory already exists." << std::endl;
+					case 183: std::cerr << "Error: " << baseDir << "... directory already exists." << std::endl;
 						break;
 					case 3: std::cerr << "ERROR: path not found." << std::endl;
 						break;
@@ -62,8 +88,9 @@ void createNewDirectory(bool &dirCheck, LPSECURITY_ATTRIBUTES access, char *dir_
 				};
 			}else{
 				// if error == 0 -> create dir func was successfull
-				std::cerr << "Directory successfully created." << std::endl;
+				std::cerr << "Directory ..." << baseDir << " .... successfully created." << std::endl;
 			}
+	}
 };
 
 int main(){
